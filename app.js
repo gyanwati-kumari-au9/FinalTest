59a5e48d8ee6875adad47a138426ddde38a33dde
Gyan <gyan@Abhisheks-MacBook-Air-63.local>
2021-04-15 10:27:01 +0530

    final bug fixed

--- app.js
+++ app.js
@@ -71,11 +71,12 @@ app.use("/groups", groupRouter);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"client/build")));
-  app.get("*",function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build","index.html"));
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-} 
+}
 
 app.listen(port,(err) => {
     if(err) throw err;
